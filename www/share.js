69d21d310ae178df1a31d417ad4cbde46c079c01
keilyn3d <keilyn2005@hotmail.com>
2018-06-06 12:05:09 -0400

    Web Share Api W3C compatibility
    Added support for w3c Web Share Api

--- www/share.js
+++ www/share.js
@@ -1,4 +1,12 @@
 module.exports = function(text,title,mimetype,success,error){
+  if(typeof text == 'object'){
+   var data = text;
+   text = data.text;
+   title = data.title;
+   mimetype = data.mimetype;
+   if(data.url)
+     text += "\n" + data.url;
+  }
   if(typeof text !== "string") {
     text = "";
   }
